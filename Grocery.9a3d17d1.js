3dcc2fa62877d9bb8eceb950a481e3e2d90d6b2d
Divyam Garg <dvm.garg26@gmail.com>
2023-12-15 12:23:01 +0530

    Updates

---
+++ Grocery.9a3d17d1.js
@@ -0,0 +1,7 @@
+var e=("undefined"!=typeof globalThis?globalThis:"undefined"!=typeof self?self:"undefined"!=typeof window?window:"undefined"!=typeof global?global:{}).parcelRequirea907;e.register("loR3J",function(o,n){Object.defineProperty(o.exports,"__esModule",{value:!0,configurable:!0}),Object.defineProperty(o.exports,"default",{get:()=>l,set:void 0,enumerable:!0,configurable:!0});// Chunking 
+// Code Splitting 
+// Dynamic Bundling 
+// lazy loading / On demand loading [] 
+var i=e("ayMG0"),l=()=>/*#__PURE__*/(0,i.jsxs)("h1",{children:[" ","Our grocery online store, we have a lot of child components inside this web page!!!"]})});//# sourceMappingURL=Grocery.9a3d17d1.js.map
+
+//# sourceMappingURL=Grocery.9a3d17d1.js.map
